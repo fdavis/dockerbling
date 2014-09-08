ea082a4865b4b73bcf4c98b16b1c07a959aedd12
fdavis <reddot357@hotmail.com>
2014-09-07 21:50:21 -0700

    updates to centos6-phab dockerfile/ansible playbook

--- dockerfiles/centos6-phabricator/playbooks/start.sh
+++ dockerfiles/centos6-phabricator/playbooks/start.sh
@@ -2,6 +2,7 @@
 /etc/init.d/sshd start
 /etc/init.d/mysqld start
 /etc/init.d/httpd start
+sudo -u apache /opt/phab/phabricator/bin/phd start
 
 # leave bash prompt so
 # 1. user can docker attach to a term
